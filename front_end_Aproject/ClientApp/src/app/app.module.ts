0e65284db71f592443c8ff6d8871bead94d11c51
slawkovvl <slawkovvl@gmail.com>
2022-10-26 16:13:06 +0200

    addind db_request component to the object

--- front_end_Aproject/ClientApp/src/app/app.module.ts
+++ front_end_Aproject/ClientApp/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { DBOperationsComponent } from './db_operations/db_operations.component';
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'db_operations', component: DBOperationsComponent }
     ])
   ],
   providers: [],
